73d437440bb43266bf6fe5a5690998d5c902d463
Abderahim <ita27rmp100@gmail.com>
2025-05-15 10:23:53 +0100

    add an array in on_screen_keyboard.py contains the kabyle alphabet

--- on_screen_keyboard.py
+++ on_screen_keyboard.py
@@ -1,2 +1,9 @@
 import tkinter as tk
 import pyautogui as pag
+# kabyle alphabet
+kabyle_alphabet = [
+    'a', 'e', 'i', 'u',
+    'b', 'd', 'f', 'g', 'ɣ', 'h', 'ḥ', 'j', 'k', 'l',
+    'm', 'n', 'p', 'q', 'r', 'ṛ', 's', 'ṣ', 't', 'ṭ',
+    'v', 'w', 'x', 'y', 'z', 'ẓ'
+]
