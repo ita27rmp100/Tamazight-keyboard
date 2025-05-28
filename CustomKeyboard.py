635ecb964c17c2df34b2eb3bb581b18c79f431c2
Abderahim <ita27rmp100@gmail.com>
2025-05-28 18:49:33 +0100

    working on custom-keyboard

--- CustomKeyboard.py
+++ CustomKeyboard.py
@@ -1,4 +1,5 @@
-import keyboard, time
+import keyboard, time, pyautogui
+from plyer import notification
 # kabyle keys
 letters = {
     "o":"ɣ",
@@ -8,15 +9,27 @@ letters = {
     "#":"ṭ",
     "`":"ẓ",
 }
-# replace the blocked key
-def new_click(n) :
-    try:
-        if n in letters.keys() :
-            time.sleep(0.1)
-            keyboard.press_and_release("backspace")
-            keyboard.write(letters[n])
-    except :
-        exit()
+
+
 # using the keyboard
-while True :
-    new_click(keyboard.read_key())
+WriteKabyle = True
+mode = ['on','off']
+def click(event) :
+    global WriteKabyle, mode
+    key = event.name
+    if key == "esc":  
+        WriteKabyle = not WriteKabyle
+        notification.notify(
+            title="Change Langauge",
+            message= f"The Kabyle keyboard is {mode[int(WriteKabyle)]}",
+            # message = "Change",
+            app_name="CustomKeyboard",
+            timeout=3
+        )
+    elif key in letters.keys() and WriteKabyle :
+        keyboard.write(letters[key])
+    else :
+        keyboard.press(key)
+# while True :
+keyboard.on_press(click)
+keyboard.wait()
