5e4b84c63450e65c4f5f256105943754b6732cf0
Abderahim <ita27rmp100@gmail.com>
2025-05-29 16:05:55 +0100

    The keyboard is done, still some explanations and illustrations such as : README file, videos and file orgnizing

--- CustomKeyboard.py
+++ CustomKeyboard.py
@@ -1,39 +1,37 @@
-import keyboard
+import keyboard , sys , os , json
 from plyer import notification
-# kabyle keys
-letters = {
-    "o":"ɣ",
-    "[":"ṭ",
-    "]":"ṛ",
-    "\\":"ḍ",
-    ";":"ǧ",
-    "'":"ḥ",
-    "c":"ṣ",
-    ",":"ẓ",
-    ".":"č",
-    "/":"ɛ",
-    "-":",",
-    "=":"."
-}
-# Functions that trait the pressesof keys
+
+# GET kabyle letters from json file
+with open(os.path.join(os.path.dirname(__file__),"kabyle.json"),mode="r",encoding="UTF-8") as Jfile : 
+    letters =  json.load(Jfile)
+# Functions that handle with the presses of keys
 WriteKabyle = True
 Upper = False
+shiftActive = False
 modes = ['off','on']
-def toogleMode() :
-    global modes , WriteKabyle
-    WriteKabyle = not WriteKabyle
+def notify(ttle,msg) :
     notification.notify(
-        title="Change Langauge",
-        message= f"The Kabyle keyboard is {modes[int(WriteKabyle)]}",
+        title=ttle,
+        message= msg,
         app_name="CustomKeyboard",
         app_icon="kabyle_keyboard.ico",
         timeout=1
     )
+def shiftDown(e) :
+    global shiftActive
+    shiftActive = not shiftActive
+def shiftUp(e) :
+    global shiftActive
+    shiftActive = not shiftActive
+def toogleMode() :
+    global modes , WriteKabyle
+    WriteKabyle = not WriteKabyle
+    notify("Change Langauge",f"The Kabyle keyboard is {modes[int(WriteKabyle)]}")
 def UpperToggle() :
     global Upper
     Upper = not Upper
 def upperWrite(v) :
-    if Upper :
+    if Upper or shiftActive :
         keyboard.write(str(v).upper())
     else :
         keyboard.write(v)
@@ -44,7 +42,10 @@ def ReplaceClick(o,v) :
     else :
         upperWrite(o)
 def stop() :
-    exit()
+    notify("Exit","The kabyle keyboard has been exited")
+    keyboard.unhook_all()
+    sys.exit()
+
 # replacement of keys :
 for k, v in letters.items() :
     keyboard.on_press_key(
@@ -52,7 +53,10 @@ for k, v in letters.items() :
         lambda e,repl = v  , origin = k: ReplaceClick(origin,repl),
         suppress=True
     )
+    
 keyboard.on_press_key("caps lock",lambda e :UpperToggle(),suppress=False)
-keyboard.on_press_key("esc",lambda e : toogleMode(),suppress=False)
 keyboard.on_press_key("end",lambda e : stop(),suppress=False)
+keyboard.on_press_key("esc",lambda e : toogleMode(),suppress=False)
+keyboard.on_press_key("shift",lambda e : shiftDown(e),suppress=False)
+keyboard.on_release_key("shift",lambda e : shiftUp(e),suppress=False)
 keyboard.wait()
