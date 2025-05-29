12a4436299c1c8e2f024548e75464c7ab70e38ab
Abderahim <ita27rmp100@gmail.com>
2025-05-29 11:31:43 +0100

    complete the settings of custom keyboard, I need only to add the rest of kabyle letters

--- CustomKeyboard.py
+++ CustomKeyboard.py
@@ -13,24 +13,26 @@ letters = {
 
 # using the keyboard
 WriteKabyle = True
-mode = ['on','off']
-def click(event) :
-    global WriteKabyle, mode
-    key = str(event.name).lower()
-    print(key)
-    if key == "esc":  
-        WriteKabyle = not WriteKabyle
-        notification.notify(
-            title="Change Langauge",
-            message= f"The Kabyle keyboard is {mode[int(WriteKabyle)]}",
-            app_name="CustomKeyboard",
-            app_icon="kabyle_keyboard.ico",
-            timeout=3
-        )
-    elif key in letters.keys() and WriteKabyle :
-        keyboard.write(letters[key])
-    else :
-        keyboard.press(key)
-# while True :
-keyboard.on_press(click)
+modes = ['off','on']
+def toogleMode() :
+    global modes , WriteKabyle
+    WriteKabyle = not WriteKabyle
+    notification.notify(
+        title="Change Langauge",
+        message= f"The Kabyle keyboard is {modes[int(WriteKabyle)]}",
+        app_name="CustomKeyboard",
+        app_icon="kabyle_keyboard.ico",
+        timeout=2
+    )
+
+
+# replacement of keys :
+for k, v in letters.items() :
+    keyboard.on_press_key(
+        k,
+        lambda e,repl = v : keyboard.write(repl),
+        suppress=True
+    )
+ɣ
+keyboard.on_press_key("esc",lambda e : toogleMode(),suppress=False)
 keyboard.wait()
